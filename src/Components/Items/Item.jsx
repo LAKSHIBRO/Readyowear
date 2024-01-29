9d75cefa6859f0ce475809ee336b6b285ea704d5
Lakshitha <notlakshitha@gmail.com>
2024-01-29 13:51:19 +0530

    Offers & Popular components created

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -8,10 +8,10 @@ const Item = (props) => {
         <p>{props.name}</p>
         <div className="item-prices">
             <div className="item-proce-new">
-                {props.new_price}
+                ${props.new_price}
             </div>
             <div className="item-proce-old">
-            {props.old_price}
+                ${props.old_price}
             </div>
         </div>
     </div>
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from '../Components/Hero/Hero';
+import Popular from '../Components/Navbar/Popular/Popular';
+import Offers from '../Components/Offers/Offers';
 
 const Shop = () => {
   return (
     <div>
       <Hero />
+      <Popular/>
+      <Offers/>
     </div>
   );
 };
